cb5699ab1094395365fc2df24043f810e6e20091
jqh <841324345@qq.com>
2020-02-18 15:54:40 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# dcat-admin-grid-sortable
+
+<div align="center">
+
+# DCAT-ADMIN GRID-SORTABLE
+
+</div>
+
+这个插件可以帮助你通过拖动数据列表的行来进行排序，前端基于[jQueryUI sortable](https://jqueryui.com/sortable/), 后端基于[eloquent-sortable](https://github.com/spatie/eloquent-sortable)。
+
+
+![Kapture 2019-06-25 at 10 14 51](https://user-images.githubusercontent.com/1479100/60064224-50b97080-9732-11e9-8023-431fc6fe81a5.gif)
+
+## Installation
+
+```shell
+composer require dcat-admin-extension/grid-sortable -vvv
+```
+
+然后打开`http://yourhost/admin/helpers/extensions`，依次点击`启用`和`导入`。
+
+## 使用
+
+修改模型
+
+```php
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
+
+class MyModel extends Model implements Sortable
+{
+    use SortableTrait;
+
+    public $sortable = [
+        'order_column_name' => 'order_column',
+        'sort_when_creating' => true,
+    ];
+}
+```
+
+在表格中使用
+
+```php
+$grid = new Grid(new MyModel());
+
+$grid->sortable();
+```
+
+## 翻译
+
+在对应的语言种类文件夹下的`global.php`中可以修改按钮文本，以简体中文为例：翻译文件是`resources/lang/zh-CN.json`：
+```php
+return [
+    'fields' => [...],
+    'labels' => [
+        ...
+        'Save order' => '保存排序',
+    ],
+];
+```
+
+License
+------------
+Licensed under [The MIT License (MIT)](LICENSE).
---
+++ src/SortableDisplay.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dcat\Admin\Extension\GridSortable;
+
+use Dcat\Admin\Admin;
+use Dcat\Admin\Grid\Displayers\AbstractDisplayer;
+
+class SortableDisplay extends AbstractDisplayer
+{
+    protected static $js = [
+        'vendor/dcat-admin-extensions/grid-sortable/jquery-ui.min.js'
+    ];
+
+    protected function script()
+    {
+        $id = $this->grid->tableId();
+
+        $script = <<<JS
+$("#{$id} tbody").sortable({
+    placeholder: "sort-highlight",
+    handle: ".grid-sortable-handle",
+    forcePlaceholderSize: true,
+    zIndex: 999999
+}).on("sortupdate", function(event, ui) {
+
+    var sorts = [];
+    $(this).find('.grid-sortable-handle').each(function () {
+        sorts.push($(this).data());
+    });
+    
+    $('#{$id}').closest('.row').first().find('.grid-save-order-btn').data('sort', sorts).show();
+});
+JS;
+        Admin::script($script);
+    }
+
+    protected function getRowSort($sortName)
+    {
+        return $this->row->{$sortName};
+    }
+
+    public function display($sortName = null)
+    {
+        $this->script();
+
+        $key  = $this->key();
+        $sort = $this->getRowSort($sortName);
+
+        return <<<HTML
+<a class="grid-sortable-handle" style="cursor:move;" data-key="{$key}" data-sort="{$sort}">
+   <i class="fa fa-ellipsis-v"></i>
+   <i class="fa fa-ellipsis-v"></i>
+</a>
+HTML;
+    }
+}
